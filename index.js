bbeb59ef39b0a9a8516da7df74e78c3ecaba035f
Shevik11 <144562073+Shevik11@users.noreply.github.com>
2023-11-15 18:35:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 const upload = multer();
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
 
-app.use(multer().none());
+app.use(multer().none()); 
 app.use(express.json());
 app.use(express.static('static'));
 
-const notes = [];
+var filename = 'notes.json';
+
+if (!fs.existsSync(filename)) {
+  // Якщо файл не існує, створюємо його з порожнім масивом
+  fs.writeFileSync(filename, '[]', 'utf8');
+}
+
+app.use(bodyParser.raw({ type: 'text/plain' }));
+
+const notes = JSON.parse(fs.readFileSync(filename, 'utf8'));
 
 app.get('/', (req, res) => {
   res.send('Сервіс запущено. Вітаємо!');
@@ -37,7 +47,6 @@ app.get('/notes/:note_name', (req, res) => {
 app.post('/upload', (req, res) => {
   const note_name = req.body.note_name;
   const note = req.body.note;
-
   const existing = notes.find(note => note.note_name === note_name);
 
   if (existing) {
@@ -46,25 +55,27 @@ app.post('/upload', (req, res) => {
     notes.push({ note_name: note_name, note: note });
 
     // Зберігаємо оновлені нотатки у форматі JSON
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
+    fs.writeFileSync(filename, JSON.stringify(notes), 'utf8');
+
+    console.log('Поточний стан notes:', notes);
 
     res.status(201).send("OK");
   }
 });
 
-app.put('/notes/:note_name', (req, res) => {
-  const note_name = req.params.note_name;
-  const note = req.body.note;
-  const noteIndex = notes.findIndex(note => note.note_name === note_name);
+app.put("/notes/:noteName", (req, res) => {
+  const noteName = req.params.noteName;
+  const noteIndex = notes.findIndex(note => note.note_name === noteName);
 
   if (noteIndex !== -1) {
-    notes[noteIndex].note = note;
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
-    res.status(200).send("OK");
+    notes[noteIndex].note = req.body.toString();
+    console.log('Поточний стан notes:', req.body.toString());
+    res.sendStatus(200);
   } else {
-    res.status(404).json("Not Found");
+    res.sendStatus(404);
   }
 });
+
 
 app.delete('/notes/:note_name', (req, res) => {
   const note_name = req.params.note_name;
@@ -72,7 +83,7 @@ app.delete('/notes/:note_name', (req, res) => {
 
   if (noteIndex !== -1) {
     notes.splice(noteIndex, 1);
-    fs.writeFileSync('notes.json', JSON.stringify(notes), 'utf8');
+    fs.writeFileSync(filename, JSON.stringify(notes), 'utf8');
     res.status(200).send('OK');
   } else {
     res.status(404).send('Not Found');
